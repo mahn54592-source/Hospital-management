a44c59add484d69ab77351d81e5ef6c7240549d3
mahn54592-source <mahn54592@gamil.com>
2026-05-09 03:12:32 +0500

    add aws rds

--- php/connect.php
+++ php/connect.php
@@ -1,10 +1,10 @@
 <?php
-$host = 'mysql';
+$host = 'mydb.cj44mq6wimjt.ap-southeast-1.rds.amazonaws.com';
 $username = 'root';
 $password = 'root';
 $dbname = 'PatientCareSystem';
 
-$conn = new mysqli($host, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname, 3306);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
